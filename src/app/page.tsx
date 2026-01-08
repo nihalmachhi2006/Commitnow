6489216d3c7f2370d627806b5b6edc690d0f0293
Nihal Machhi <nihalmachhi11@gmail.com>
2026-01-08 16:59:47 +0530

    ReCheck Production

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,8 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <h1>Deploy Sucessfully : 200</h1>
+      <h1>Push To Production successfully : 200</h1>
+      <h2>Jan-2026</h2>
     </div>
   );
 }
